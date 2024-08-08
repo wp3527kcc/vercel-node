740c56ebb7e828976ced7756fc5480aa658f5ef5
hezhongfeng <287645587@qq.com>
2024-08-08 11:03:12 +0800

    🎉 init(init):

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { config } from 'dotenv';
+// 读取 .env 文件
+const envConfig = config();
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/t')
+  getHelloo(): string {
+    return envConfig.parsed.DB_HOST || 'tttt';
+  }
+}
