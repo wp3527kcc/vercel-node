bee912ca586210cb797a404699d8d045819b3b26
hezhongfeng <287645587@qq.com>
2024-08-08 13:45:55 +0800

    ✨ feat(add pg):

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import { Userr } from './user.entity';
+
+// 读取 .env 文件
+const envConfig = config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ssl: Boolean(envConfig.parsed.DB_SSL || process.env.DB_SSL),
+      type: 'postgres',
+      host: envConfig.parsed.DB_HOST || process.env.DB_HOST,
+      port: Number(envConfig.parsed.DB_PORT || process.env.DB_PORT),
+      username: envConfig.parsed.DB_USER || process.env.DB_USER,
+      password: envConfig.parsed.DB_PASSWORD || process.env.DB_PASSWORD,
+      database: envConfig.parsed.DB_DB || process.env.DB_DB,
+      entities: [Userr],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
