ef7926e8d32a82c4bf8bd638b033da7270957bf2
zhongke <719045449@qq.com>
2025-05-24 18:36:06 +0800

    feat: modify front/package.json

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://vercel-node-ruby-nine.vercel.app/file/vercel-node/'
 })
