6585cbec860e513200277a1412bf22feedbdd711
zhongke <zhongke@mail.tuchong.com>
2025-06-10 18:57:24 +0800

    feat: retry vercel front

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://vercel-node-ruby-nine.vercel.app/api/file/vercel-node/'
+  base: 'https://vercel-node-ruby-nine.vercel.app/api/file/github/'
 })
